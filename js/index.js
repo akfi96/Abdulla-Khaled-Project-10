7b34dc0e2a66c4f1105d90728556b757c6ee69a3
akfi96 <akfi96@gmail.com>
2024-06-13 21:35:28 +0300

    first commit
    13-6-2024

---
+++ js/index.js
@@ -0,0 +1,49 @@
+// sign up inputs
+var nameSignUp = document.getElementById("nameSignUp");
+var emailSignUp = document.getElementById("emailSignUp");
+var passSignUp = document.getElementById("passSignUp");
+// sign in inputs
+var emailSignIn = document.getElementById("emailSignIn");
+var passSignIn = document.getElementById("passSignIn");
+// welcome name
+var welcomeName = document.getElementById("welcomeName");
+
+var userInfoList = [];
+
+function add(){
+    // var inputs = { name: nameSignUp.value , email: emailSignUp.value , pass: passSignUp.value }
+    // userInfoList.push(inputs)
+    var inputs = { name: nameSignUp.value , pass: passSignUp.value }
+    localStorage.setItem(emailSignUp.value, JSON.stringify(inputs))
+    // console.log(userInfoList);
+}
+function lookup(){
+    var email = emailSignUp.value
+    if(localStorage.getItem(email) === null ){
+        add();
+    }
+    else{
+        window.alert("This email already exists")
+    }
+}
+function signinlookup(){
+    var email = emailSignIn.value
+    var pass = passSignIn.value
+    if(localStorage.getItem(email) === null ){
+        window.alert("This email doesn't exist")
+    }
+    else{
+        var userinfo = JSON.parse(localStorage.getItem(email))
+        var name = userinfo.name
+        console.log(userinfo.pass);
+        if (userinfo.pass == pass){
+            welcomeName.innerHTML = `${name}`
+            window.location.assign("welcome.html")
+            // window.alert("pass is correct")
+        }
+        else{
+            window.alert("pass is wrong")
+        }
+    }
+}
+// console.log(localStorage.getItem("users"));
